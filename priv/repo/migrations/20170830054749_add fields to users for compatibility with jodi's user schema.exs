39f47b691e61e5a4bb5f0048e36e8ec2b2d41e65
sirfitz <sirromariof@gmail.com>
2017-09-08 20:45:40 -0500

    minor changes

--- priv/repo/migrations/20170830054749_add fields to users for compatibility with jodi's user schema.exs
+++
@@ -1,9 +0,0 @@
-defmodule :"Elixir.Blog.Repo.Migrations.Add fields to users for compatibility with jodi's user schema" do
-  use Ecto.Migration
-
-  def change do
-    alter table(:users) do
-      add :name, :string
-    end
-  end
-end
